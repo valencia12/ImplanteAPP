22b76bff350860ac373f64456457aa6547d53e54
Jorge <valencia12@github.com>
2019-06-25 15:14:39 -0600

    Add sounds of animals for the patient

--- app/src/main/java/com/example/appicoclear/activites/viewsAccounts/HomeUser.kt
+++ app/src/main/java/com/example/appicoclear/activites/viewsAccounts/HomeUser.kt
@@ -1,13 +1,21 @@
 package com.example.appicoclear.activites.viewsAccounts
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.appicoclear.R
+import com.example.appicoclear.activites.loginAction.LoginActivity
+import com.example.appicoclear.activites.viewsAccounts.actionTherapist.playSounds
+import kotlinx.android.synthetic.main.activity_home_user.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeUser : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_user)
+        playSounds1.setOnClickListener(){
+            startActivity(Intent(this, playSounds::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/appicoclear/activites/viewsAccounts/actionTherapist/playSounds.kt
+++ app/src/main/java/com/example/appicoclear/activites/viewsAccounts/actionTherapist/playSounds.kt
@@ -1,13 +1,167 @@
 package com.example.appicoclear.activites.viewsAccounts.actionTherapist
 
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import com.example.appicoclear.R
+import kotlinx.android.synthetic.main.activity_play_sounds.*
 
 class playSounds : AppCompatActivity() {
 
+    private var mediaPlayer: MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_sounds)
+
+
+        play_1.setOnTouchListener() {_,event ->
+            handleTouch(event)
+            true
+        }
+        play_2.setOnTouchListener() {_,event1 ->
+            handleTouch1(event1)
+            true
+        }
+        play_3.setOnTouchListener() {_,event2 ->
+            handleTouch2(event2)
+            true
+        }
+        play_4.setOnTouchListener() {_,event3 ->
+            handleTouch3(event3)
+            true
+        }
+        play_5.setOnTouchListener() {_,event4 ->
+            handleTouch4(event4)
+            true
+        }
+        play_6.setOnTouchListener() {_,event5 ->
+            handleTouch5(event5)
+            true
+        }
+
     }
+
+    private fun handleTouch(event:MotionEvent){
+        mediaPlayer = MediaPlayer.create(this,R.raw.guepardo)
+        mediaPlayer?.setOnPreparedListener{
+            println("Ready to go")
+        }
+        when(event.action){
+            MotionEvent.ACTION_DOWN ->{
+                println("down")
+                mediaPlayer?.start()
+            }
+            MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP ->{
+                println("up or cancel")
+                mediaPlayer?.pause()
+                mediaPlayer?.seekTo(0)
+            }
+            else ->{
+                println("other")
+            }
+        }
+    }
+    private fun handleTouch1(event1:MotionEvent){
+        mediaPlayer = MediaPlayer.create(this,R.raw.leon)
+        mediaPlayer?.setOnPreparedListener{
+            println("Ready to go")
+        }
+        when(event1.action){
+            MotionEvent.ACTION_DOWN ->{
+                println("down")
+                mediaPlayer?.start()
+            }
+            MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP ->{
+                println("up or cancel")
+                mediaPlayer?.pause()
+                mediaPlayer?.seekTo(0)
+            }
+            else ->{
+                println("other")
+            }
+        }
+    }
+    private fun handleTouch2(event2:MotionEvent){
+        mediaPlayer = MediaPlayer.create(this,R.raw.mono)
+        mediaPlayer?.setOnPreparedListener{
+            println("Ready to go")
+        }
+        when(event2.action){
+            MotionEvent.ACTION_DOWN ->{
+                println("down")
+                mediaPlayer?.start()
+            }
+            MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP ->{
+                println("up or cancel")
+                mediaPlayer?.pause()
+                mediaPlayer?.seekTo(0)
+            }
+            else ->{
+                println("other")
+            }
+        }
+    }
+    private fun handleTouch3(event3:MotionEvent){
+        mediaPlayer = MediaPlayer.create(this,R.raw.oso)
+        mediaPlayer?.setOnPreparedListener{
+            println("Ready to go")
+        }
+        when(event3.action){
+            MotionEvent.ACTION_DOWN ->{
+                println("down")
+                mediaPlayer?.start()
+            }
+            MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP ->{
+                println("up or cancel")
+                mediaPlayer?.pause()
+                mediaPlayer?.seekTo(0)
+            }
+            else ->{
+                println("other")
+            }
+        }
+    }
+    private fun handleTouch4(event4:MotionEvent){
+        mediaPlayer = MediaPlayer.create(this,R.raw.perro)
+        mediaPlayer?.setOnPreparedListener{
+            println("Ready to go")
+        }
+        when(event4.action){
+            MotionEvent.ACTION_DOWN ->{
+                println("down")
+                mediaPlayer?.start()
+            }
+            MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP ->{
+                println("up or cancel")
+                mediaPlayer?.pause()
+                mediaPlayer?.seekTo(0)
+            }
+            else ->{
+                println("other")
+            }
+        }
+    }
+    private fun handleTouch5(event5:MotionEvent){
+        mediaPlayer = MediaPlayer.create(this,R.raw.puma)
+        mediaPlayer?.setOnPreparedListener{
+            println("Ready to go")
+        }
+        when(event5.action){
+            MotionEvent.ACTION_DOWN ->{
+                println("down")
+                mediaPlayer?.start()
+            }
+            MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP ->{
+                println("up or cancel")
+                mediaPlayer?.pause()
+                mediaPlayer?.seekTo(0)
+            }
+            else ->{
+                println("other")
+            }
+        }
+    }
+
 }
+
