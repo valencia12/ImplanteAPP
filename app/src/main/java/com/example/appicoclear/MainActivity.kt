0e5bf93f90e8491050094b327ab39bb8d3cc7e45
portillo93 <00078415@uca.edu.sv>
2019-06-25 04:28:34 -0600

    Androidx

--- app/src/main/java/com/example/appicoclear/MainActivity.kt
+++ app/src/main/java/com/example/appicoclear/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.appicoclear
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
